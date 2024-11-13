aa0b81b2d72073eeee1747a53d4fcd337b474833
meow <2149268560@qq.com>
2024-11-14 02:59:08 +0800

    websocket 1.0.5

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,25 @@
 import typescript from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import terser  from '@rollup/plugin-terser'
 import { defineConfig } from 'rollup'
 
 const createRollupConfig = (packageName) =>
   defineConfig({
     input: 'src/index.ts',
-    output: {
-      file: 'dist/index.js',
-      format: 'umd',
-      name: packageName
-    },
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'umd',
+        name: packageName
+      },
+      {
+        file: 'dist/index.min.js',
+        format: 'umd',
+        name: packageName,
+        plugins: [terser()]
+      }
+    ],
     plugins: [
       resolve(),
       commonjs(),
